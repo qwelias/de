6033df7ea2ffecb2f26b4ae81af110e39689301b
Ilia Baryshnikov <qwelias@gmail.com>
2026-05-25 16:14:21 +0000

    dwm

--- dwm/patch/bar_system_stats.h
+++ dwm/patch/bar_system_stats.h
@@ -11,16 +11,6 @@ typedef struct CpuStat {
 static int width_system_stats(Bar *bar, BarArg *a);
 static int draw_system_stats(Bar *bar, BarArg *a);
 static int click_system_stats(Bar *bar, Arg *arg, BarArg *a);
-
-static int read_cpu_stat(CpuStat *out);
-
-static int calculate_cpu_usage_percent(
-    const CpuStat *prev,
-    const CpuStat *current
-);
-
-static int read_mem_usage_percent(void);
-
 static void system_stats_alrm(void);
 
 #endif
